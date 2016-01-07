59507f9cc2ad1b02f78d1f05ec5ff6468eae53a3
Aaron Nwabuoku <aerisvulpe@gmail.com>
2016-01-07 12:44:35 -0500

    Completed week two assignments.

--- src/document/DocumentBenchmarking.java
+++ src/document/DocumentBenchmarking.java
@@ -4,7 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 
-/** A class for timing the EfficientDocument and BasicDocument classes
+/**
+ * A class for timing the EfficientDocument and BasicDocument classes
  * 
  * @author UC San Diego Intermediate Programming MOOC team
  *
@@ -12,89 +13,103 @@ import java.io.InputStreamReader;
 
 public class DocumentBenchmarking {
 
-	
-	public static void main(String [] args) {
+	public static void main(String[] args) {
 
-	    // Run each test more than once to get bigger numbers and less noise.
-	    // You can try playing around with this number.
-	    int trials = 100;
+		// Run each test more than once to get bigger numbers and less noise.
+		// You can try playing around with this number.
+		int trials = 100;
 
-	    // The text to test on
-	    String textfile = "data/warAndPeace.txt";
-		
-	    // The amount of characters to increment each step
-	    // You can play around with this
+		// The text to test on
+		String textfile = "data/warAndPeace.txt";
+
+		// The amount of characters to increment each step
+		// You can play around with this
 		int increment = 20000;
 
-		// The number of steps to run.  
+		// The number of steps to run.
 		// You can play around with this.
 		int numSteps = 20;
-		
-		// THe number of characters to start with. 
+
+		// THe number of characters to start with.
 		// You can play around with this.
 		int start = 50000;
-		
-		// TODO: Fill in the rest of this method so that it runs two loops
-		// and prints out timing results as described in the assignment 
+
+		// Fill in the rest of this method so that it runs two loops
+		// and prints out timing results as described in the assignment
 		// instructions.
-		for (int numToCheck = start; numToCheck < numSteps*increment + start; 
-				numToCheck += increment)
-		{
-			// numToCheck holds the number of characters that you should read from the 
-			// file to create both a BasicDocument and an EfficientDocument.  
+		for (int numToCheck = start; numToCheck < numSteps * increment + start; numToCheck += increment) {
+			// numToCheck holds the number of characters that you should read
+			// from the
+			// file to create both a BasicDocument and an EfficientDocument.
+
+			/*
+			 * Each time through this loop you should: 1. Print out numToCheck
+			 * followed by a tab (\t) (NOT a newline) 2. Read numToCheck
+			 * characters from the file into a String Hint: use the helper
+			 * method below. 3. Time a loop that runs trials times (trials is
+			 * the variable above) that: a. Creates a BasicDocument b. Calls
+			 * fleshScore on this document 4. Print out the time it took to
+			 * complete the loop in step 3 (on the same line as the first print
+			 * statement) followed by a tab (\t) 5. Time a loop that runs trials
+			 * times (trials is the variable above) that: a. Creates an
+			 * EfficientDocument b. Calls fleshScore on this document 6. Print
+			 * out the time it took to complete the loop in step 5 (on the same
+			 * line as the first print statement) followed by a newline (\n)
+			 */
+			System.out.print(numToCheck + "\t");
+			String numToCheckChars = getStringFromFile(textfile, numToCheck);
+
+			BasicDocument basicDocument = new BasicDocument(numToCheckChars);
+			long basicDocStartTime = System.nanoTime();
+			for (int i = 0; i < trials; i++)
+				basicDocument.getFleschScore();
+			long basicDocEndTime = System.nanoTime();
+
+			EfficientDocument efficientDocument = new EfficientDocument(numToCheckChars);
+			long efficientDocStartTime = System.nanoTime();
+			for (int i = 0; i < trials; i++)
+				efficientDocument.getFleschScore();
+			long efficientDocEndTime = System.nanoTime();
 			
-			/* Each time through this loop you should:
-			 * 1. Print out numToCheck followed by a tab (\t) (NOT a newline)
-			 * 2. Read numToCheck characters from the file into a String
-			 *     Hint: use the helper method below.
-			 * 3. Time a loop that runs trials times (trials is the variable above) that:
-			 *     a. Creates a BasicDocument 
-			 *     b. Calls fleshScore on this document
-			 * 4. Print out the time it took to complete the loop in step 3 
-			 *      (on the same line as the first print statement) followed by a tab (\t)
-			 * 5. Time a loop that runs trials times (trials is the variable above) that:
-			 *     a. Creates an EfficientDocument 
-			 *     b. Calls fleshScore on this document
-			 * 6. Print out the time it took to complete the loop in step 5 
-			 *      (on the same line as the first print statement) followed by a newline (\n) 
-			 */  
-			 
+			System.out.print((basicDocEndTime - basicDocStartTime)/1000000000.0 + "\t");
+			System.out.println((efficientDocEndTime - efficientDocStartTime)/1000000000.0);
 		}
-	
+
 	}
-	
-	/** Get a specified number of characters from a text file
+
+	/**
+	 * Get a specified number of characters from a text file
 	 * 
-	 * @param filename The file to read from
-	 * @param numChars The number of characters to read
-	 * @return The text string from the file with the appropriate number of characters
+	 * @param filename
+	 *            The file to read from
+	 * @param numChars
+	 *            The number of characters to read
+	 * @return The text string from the file with the appropriate number of
+	 *         characters
 	 */
 	public static String getStringFromFile(String filename, int numChars) {
-		
+
 		StringBuffer s = new StringBuffer();
 		try {
-			FileInputStream inputFile= new FileInputStream(filename);
+			FileInputStream inputFile = new FileInputStream(filename);
 			InputStreamReader inputStream = new InputStreamReader(inputFile);
 			BufferedReader bis = new BufferedReader(inputStream);
 			int val;
 			int count = 0;
 			while ((val = bis.read()) != -1 && count < numChars) {
-				s.append((char)val);
+				s.append((char) val);
 				count++;
 			}
 			if (count < numChars) {
 				System.out.println("Warning: End of file reached at " + count + " characters.");
 			}
 			bis.close();
+		} catch (Exception e) {
+			System.out.println(e);
+			System.exit(0);
 		}
-		catch(Exception e)
-		{
-		  System.out.println(e);
-		  System.exit(0);
-		}
-		
-		
+
 		return s.toString();
 	}
-	
+
 }
